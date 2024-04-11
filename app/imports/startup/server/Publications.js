af4f320e91d45588868da58d9cbe63b3d490b4a5
Vanessa Nishi <vanessa.nishi2021@gmail.com>
2024-04-10 17:24:22 -1000

    base setup for card collection
    - figure out how to make default cards  available to all users, not just john@foo.com

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { TCards } from '../../api/tcard/TCard';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
@@ -17,6 +18,21 @@ Meteor.publish(Stuffs.userPublicationName, function () {
 Meteor.publish(Stuffs.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.collection.find();
+  }
+  return this.ready();
+});
+
+Meteor.publish(TCards.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return TCards.collection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish(TCards.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return TCards.collection.find();
   }
   return this.ready();
 });
---
+++ app/imports/ui/components/TCardItem.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
+import { Card, Image, Row, Col } from 'react-bootstrap';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const TCardItem = ({ tcard }) => (
+  <Card className="h-100 text-center">
+    <Card.Header>
+      <Card.Title>{tcard.name}</Card.Title>
+    </Card.Header>
+    <Card.Body>
+      <Image width={75} src={tcard.image} />
+      <Card.Text><h5>{tcard.rank}</h5></Card.Text>
+      <Card.Text>
+        <Row>
+          <Col>Course: {tcard.course}</Col>
+          <Col>Power: {tcard.power}</Col>
+        </Row>
+      </Card.Text>
+      <Card.Text><strong>Special Move: {tcard.smtitle}</strong></Card.Text>
+      <Card.Text>{tcard.smdescription}</Card.Text>
+      {/* <Link to={`/edit/${tcard._id}`}>Edit</Link> */}
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+TCardItem.propTypes = {
+  tcard: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    rank: PropTypes.string,
+    course: PropTypes.string,
+    power: PropTypes.number,
+    smtitle: PropTypes.string,
+    smdescription: PropTypes.string,
+    owner: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default TCardItem;
