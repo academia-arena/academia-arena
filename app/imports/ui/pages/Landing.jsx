703c0eab98c64d53a639a2811c5024de5670dcda
Michaella Villanueva <mvchaella@gmail.com>
2024-04-11 11:07:48 -1000

    Fix Errors

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,7 +4,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
-    <div className="partition-bar"></div>
+    <div className="partition-bar" />
     <div id="landing-page-top" className="py-3 align-middle">
       <Col fluid className="align-middle text-center">
         <Row id="landing-page-title" className="d-flex flex-column justify-content-center">
@@ -16,11 +16,12 @@ const Landing = () => (
         </Row>
       </Col>
     </div>
-    <div className="partition-bar"></div>
+    <div className="partition-bar" />
     <Container>
       <Row id="landing-page-about">
         <h2>About Academia Arena</h2>
         <p>The purpose of this web application is to help incoming students or continuing ICS students, especially those who feel disconnected from their professors, connect with them and learn about them more easily.</p>
+        {/* eslint-disable-next-line max-len */}
         <p>Inspired by trading card games such as Magic: The Gathering, Pokemon, Yu-Gi-Oh Academia Arena: ICS Edition revolves around collectible cards that contain information about the professors on the UH Manoa campus. The information on the card includes a headshot image, the version of the card (ex: “ICS 314 Spring 2024”, “ICS 311 Fall 2023”, etc.), fun facts, projects they may have worked on, and the rarity of the card.</p>
         <p>This platform provides an opportunity for students to meet and interact with others who may have the same class, providing an alleyway to find friends with shared academic interests or professors.</p>
       </Row>
