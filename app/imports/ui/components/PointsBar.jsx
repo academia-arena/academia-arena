7b35cb1e408627483082f01235aa68d49f4a63fa
Michaella Villanueva <mvchaella@gmail.com>
2024-04-23 17:49:52 -1000

    eslint check

--- app/imports/ui/components/PointsBar.jsx
+++ app/imports/ui/components/PointsBar.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react';
 
-//code adapted from Katsiaryna Lupachova on GitHub
+// code adapted from Katsiaryna Lupachova on GitHub
 const PointsBar = (props) => {
+  // eslint-disable-next-line react/prop-types
   const { bgcolor, completed } = props;
   const containerStyles = {
     height: 20,
     width: '100%',
-    backgroundColor: "#e0e0de",
+    backgroundColor: '#e0e0de',
     borderRadius: 50,
     margin: 25,
-  }
+  };
 
   const fillerStyles = {
     height: '100%',
@@ -18,13 +19,13 @@ const PointsBar = (props) => {
     transition: 'width 1s ease-in-out',
     borderRadius: 'inherit',
     textAlign: 'right',
-  }
+  };
 
   const labelStyles = {
     padding: 5,
     color: 'white',
     fontWeight: 'bold',
-  }
+  };
 
   return (
     <div style={containerStyles}>
