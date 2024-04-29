a1bbf8e50cc30dc5680b13622393dc059d238d93
Vanessa Nishi <vanessa.nishi2021@gmail.com>
2024-04-28 16:16:20 -1000

    implement search bar functionality, create one card component that can be used for all three styles of cards

---
+++ app/imports/ui/components/Card.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
+import { Card, Col, Container, Image, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Roles } from 'meteor/alanning:roles';
+/** Renders a single row in the List card (Admin) table. See pages/AdminPage.jsx. */
+const ThisCard = ({ card, background, title, image, text }) => (
+  <Card id={background} className="align-items-center my-2" style={{ width: '370px', height: '570px' }}>
+    <Card.Title id={title}>
+      <Container className="text-center">
+        <strong>{card.role} {card.firstName} {card.lastName}</strong>
+        <div>{card.cardName}</div>
+      </Container>
+    </Card.Title>
+    <Card.Body id={image} className="text-center">
+      <Image src={card.image} height="150px" />
+    </Card.Body>
+    <Card.Text id={text}>
+      <Row className="text-center">
+        <th style={{ fontSize: '20px' }}>{card.type} {card.role}</th>
+      </Row>
+      <Row className="ps-5">
+        <Col>Course: {card.course}</Col>
+        <Col>Power: {card.power}</Col>
+      </Row>
+      <Container>
+        <th className="py-2">Special Move: {card.title}</th>
+        {card.description}
+        <th className="py-2">Fun Fact: {card.funFact}</th>
+        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+          <Link to={`/edit/${card._id}`}>Edit</Link>
+        ) : (<Link to={`/listtrade/${card._id}`}>Trade</Link>)}
+      </Container>
+    </Card.Text>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+ThisCard.propTypes = {
+  card: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    course: PropTypes.string,
+    type: PropTypes.string,
+    role: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    funFact: PropTypes.string,
+    cardName: PropTypes.string,
+    image: PropTypes.string,
+    power: PropTypes.number,
+    _id: PropTypes.string,
+    owner: PropTypes.string,
+    isListedForTrade: PropTypes.string,
+    backgroundStyle: PropTypes.string,
+    titleStyle: PropTypes.string,
+    imageStyle: PropTypes.string,
+    textStyle: PropTypes.string,
+  }).isRequired,
+};
+
+ThisCard.propTypes = {
+  background: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+export default ThisCard;
--- app/imports/ui/pages/ListTCard.jsx
+++ app/imports/ui/pages/ListTCard.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TCards } from '../../api/tcard/TCard';
 import LoadingSpinner from '../components/LoadingSpinner';
-import CommonCard from '../components/Common-Card';
-import RareCard from '../components/Rare-Card';
-import LegendaryCard from '../components/Legendary-Card';
+import ThisCard from '../components/Card';
+
+/* Search bar code from tutorial by Carmelle Codes on Youtube */
+const getFilteredCards = (query, tcards) => {
+  if (!query) {
+    return tcards;
+  }
+  return tcards.filter((card) => card.firstName.includes(query) || card.firstName.toLowerCase().includes(query) || card.lastName.includes(query) || card.lastName.toLowerCase().includes(query));
+};
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListTCard = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const [query, setQuery] = useState('');
   const { ready, tcards } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
@@ -25,6 +32,22 @@ const ListTCard = () => {
       ready: rdy,
     };
   }, []);
+  const filteredCards = getFilteredCards(query, tcards);
+  const commonBackground = 'Cards1';
+  const commonTitle = 'CardsTitle1';
+  const commonImage = 'CardsImage1';
+  const commonText = 'CardsText1';
+  const rareBackground = 'Cards2';
+  const rareTitle = 'CardsTitle2';
+  const rareImage = 'CardsImage2';
+  const rareText = 'CardsText2';
+  const legendaryBackground = 'Cards3';
+  const legendaryTitle = 'CardsTitle3';
+  const legendaryImage = 'CardsImage3';
+  const legendaryText = 'CardsText3';
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
   return (ready ? (
     <Col id="view-collection-page">
       <Container fluid className="py-3" id="title-block">
@@ -34,17 +57,17 @@ const ListTCard = () => {
       </Container>
       <Container className="py-3 justify-content-center">
         <Row className="py-3 d-flex justify-content-center">
-          <InputGroup id="search">
-            <Form.Control id="SearchBar" placeholder="Name" />
-            <Button variant="success">Search</Button>
-          </InputGroup>
-        </Row>
-        <Row style={{ paddingLeft: '6vh' }} className="py-3 gx-2 justify-content-center">
-          {tcards.map((tcard) => {
-            if (tcard.type === 'Common') return <Col><CommonCard key={tcard._id} card={tcard} /></Col>;
-            if (tcard.type === 'Rare') return <Col><RareCard key={tcard._id} card={tcard} /></Col>;
-            return <Col><LegendaryCard key={tcard._id} card={tcard} /></Col>;
-          })}
+          <input type="text" onChange={handleChange} />
+          <Row style={{ paddingLeft: '6vh' }} className="py-3 gx-2 justify-content-center">
+            {
+              filteredCards.map((tcard) => {
+                if (tcard.type === 'Common') return <Col><ThisCard key={tcard._id} card={tcard} background={commonBackground} title={commonTitle} image={commonImage} text={commonText} /></Col>;
+                if (tcard.type === 'Rare') return <Col><ThisCard key={tcard._id} card={tcard} background={rareBackground} title={rareTitle} image={rareImage} text={rareText} /></Col>;
+                return <Col><ThisCard key={tcard._id} card={tcard} background={legendaryBackground} title={legendaryTitle} image={legendaryImage} text={legendaryText} /></Col>;
+              })
+
+            }
+          </Row>
         </Row>
       </Container>
     </Col>
