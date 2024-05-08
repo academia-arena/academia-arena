ea5ba0d5f52ca473e7080da9d840462eae8c3c64
Erin Patterson <erinlpat@hawaii.edu>
2024-05-07 22:51:07 -1000

    fix

--- app/imports/api/allcard/AllCard.js
+++ app/imports/api/allcard/AllCard.js
@@ -35,6 +35,7 @@ class AllCardsCollection {
     this.collection.attachSchema(this.schema);
     // Define names for publications and subscriptions
     this.userPublicationName = `${this.name}.publication.user`;
+    this.userAllPublicationName = `${this.name}.publication.user`;
     this.adminPublicationName = `${this.name}.publication.admin`;
   }
 }
